4aa79068da485b7df05fc6e00a38cfce36641776
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-20 16:07:02 +0530

    adding express files

---
+++ Members.js
@@ -0,0 +1,8 @@
+const members = [
+  { id: 1, name: "sayan", email: "sayan@gmail.com", status: "active" },
+  { id: 2, name: "soumi", email: "soumi@gmail.com", status: "active" },
+  { id: 3, name: "bebun", email: "bebun@gmail.com", status: "inactive" },
+  { id: 4, name: "tumba", email: "tumba@gmail.com", status: "inactive" },
+];
+
+module.exports = members;
---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+// const logger = require("./middleware/logger");
+
+const app = express();
+
+//Apply Middleware
+//app.use(logger);
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+
+//set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//members Api route
+app.use("/api/members", require("./routes/api/members"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
